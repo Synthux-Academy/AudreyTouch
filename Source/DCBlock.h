8adf20fd4d855e6c4400d11073f0239edf41ca1b
AlbertoBerera <berera.alberto@gmail.com>
2025-12-07 19:05:38 +0100

    added dc block references

--- Source/DCBlock.h
+++ Source/DCBlock.h
@@ -1,14 +1,32 @@
+/*
+Copyright (c) 2020 Electrosmith, Corp
+
+Use of this source code is governed by an MIT-style
+license that can be found in the LICENSE file or at
+https://opensource.org/licenses/MIT.
+*/
+
 #pragma once
 
 namespace infrasonic
 {
+/** Removes DC component of a signal
+    */
 class DCBlock
 {
   public:
+    DCBlock(){};
+    ~DCBlock(){};
+
+    /** Initializes DCBlock module
+    */
     void Init(float sample_rate);
+
+    /** performs DCBlock Process 
+    */
     float Process(float in);
 
   private:
     float input_, output_, gain_;
 };
-}
+} // namespace infrasonic
